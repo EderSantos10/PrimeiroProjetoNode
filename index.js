e7b8b6ad4c3aecf35992b1b94c6263537d63d6ae
Eder Vinicius <eder.e10@hotmail.com>
2024-06-21 20:56:12 -0300

    Inserindo Criação de Contas

--- index.js
+++ index.js
@@ -19,6 +19,53 @@ function operation(){
     ]).then((answer) => {
         const action = answer['action']
 
-        console.log(action)
+        if(action === 'Criar Conta'){
+            createAccount()
+        }
     }).catch(err => console.log(err))
+}
+
+// creat an account
+function createAccount() {
+    console.log(ansi.bgGreen.black('Parabéns por escolher o nosso banco!'))
+    console.log(ansi.green('Defina as condições de sua conta a seguir'))
+
+    buildAccount()
+}
+
+function buildAccount() {
+
+    inquirer.prompt([
+        {
+            name: 'accountName',
+            message: 'Digite um nome para sua conta:'
+        },
+    ])
+    .then((answer) => {
+        const accountName = answer['accountName']
+
+        console.info(accountName)
+
+        if(!fs.existsSync('accounts')) {
+            fs.mkdirSync('accounts')
+            }
+
+        if(fs.existsSync(`accounts/${accountName}.json`)) {
+            console.log(ansi.bgRed.black('Esta conta ja existe, escolha outro nome:'),
+           )
+           buildAccount()
+           return
+        }
+
+        fs.writeFileSync(`accounts/${accountName}.json`, '{"balance": 0}', 
+            function (err) { 
+                console.log(err)
+            }, 
+        )
+
+        console.log(ansi.green('Parabéns, a sua conta foi criada com sucesso!'))
+
+        operation()
+    })
+    .catch(err => console.log(err))
 }
